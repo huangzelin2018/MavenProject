edaf51dead136f1058912b901b144aad7232d254
huangzl <huang_ze_lin@163.com>
2018-05-03 23:30:44 +0800

    更新

--- easyframework-admin/src/main/java/com/easyframework/EasyframeworkApplication.java
+++ easyframework-admin/src/main/java/com/easyframework/EasyframeworkApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class EasyframeworkApplication {
 
 	public static void main(String[] args) {
